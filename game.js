2ecd1fb29ab904285800798836b5a4ee84c4a22e
dreyhiflden <dreyhiflden@gmail.com>
2019-01-21 12:20:58 +0200

    Minor change

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ function getRandomArray() {
   let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   let getRandomNumber = () => {
-    return Math.floor(Math.random() * 11);
+    return Math.floor(Math.random() * 10);
   };
 
   while (arr.length >= 5) {
